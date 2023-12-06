2d3245d8170088317aaf4791203c570f358fd145
Casey Glidewell <caseyglidewell@gmail.com>
2023-12-05 20:21:59 -0800

    Updated 10000 to version 2, updated readme, and ensured provided version 2 sims are possible to achieve.

--- ten_thousand/README.md
+++ ten_thousand/README.md
@@ -1,12 +1,13 @@
 
-LAB - Class 06
-Project: Ten Thousand Version 1
+LAB - Class 07
+Project: Ten Thousand Version 2
 Author: Casey Glidewell
 
 How to initialize/run your application (where applicable)
 
-    the core file is located in the ten_thousand folder, game_logic.py, though version 1 does not have any behavior created for being run directly by the user, as v1 is about making the initial test suite pass.
+    Running game_logic.py in the ten_thousand folder will begin the game in your console.
 
 Tests
 
     Tests are run through pytest, and v1 of this assignment came with a premade set of tests to pass before version one is considered complete.
+    v2 required the ability to match the "simulations" provided in the version_2 folder. Counting the rounds, adding up score, calculating current hand, and the ability to quit at any time with q were all requirements of v2.
--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,4 +1,4 @@
-
+#
 #! IMPORTANT: Version one of the scoring system for this program was written through CHATGPT3.5 prompt engineering. Full list of commands given is included in prompt.md, as well as reasoning for this approach.
 import random
 
@@ -49,7 +49,25 @@ class GameLogic:
                 score = 1500
             elif dice == (1, 1, 1, 1, 1, 1):
                 score = 4000
-            
+
+            elif dice == (1, 1, 1, 1):
+                score = 2000
+
+            elif dice == (5, 5, 5, 5):
+                score = 1000
+
+            elif dice == (1, 1, 1, 1, 1):
+                score = 3000
+
+            elif dice == (5, 5, 5, 5, 5):
+                score = 1500
+
+            elif dice == (5, 5, 5, 5, 5, 5):
+                score = 2000
+
+            elif dice == (2, 2, 3, 3, 6, 6):
+                score = 1500
+
             # Add score for two ones
             if dice_counts[0] == 2:
                 score += 200
@@ -59,3 +77,96 @@ class GameLogic:
     @staticmethod
     def roll_dice(num_dice):
         return tuple(random.randint(1, 6) for _ in range(num_dice))
+
+    @staticmethod
+    def display_tuple(tuple):
+        output = " "
+        for i in tuple:
+            output = output + str(i)
+            output = output + " "
+        return output
+
+    # TODO: Do an elimination passover comparing hold request against available, and if hold request can match/eliminate itself fully from available, then the hold request is valid.
+    @staticmethod
+    def check_cheating(hold_request, available):
+        pass
+
+    @staticmethod
+    def startup_prompt():
+        print(f"Welcome to Ten Thousand")
+        print(f"(y)es to play or (n)o to decline")
+        response = input("> ")
+
+        if response == "n":
+            print(f"OK. Maybe another time")
+            exit()
+
+    @staticmethod
+    def play_game():
+        round_count = 1
+        die_count = 6
+        response = ""
+        total_score = 0
+        hand_score = 0
+        held_dice = []
+        playing = True
+        cheater = False
+
+        # ?Main loop
+        while playing == True:
+            # Round begins
+            this_roll = GameLogic.roll_dice(die_count)
+            print(f"Starting round {round_count}\nRolling {die_count}...")
+            print(f"***{GameLogic.display_tuple(this_roll)}***")
+            print(f"Enter dice to keep, or (q)uit:")
+
+            response = input("> ")
+
+            if response == "q":
+                print(f"Thanks for playing. You earned {total_score} points")
+                exit()
+
+            # if GameLogic.check_cheating(response, this_roll) == True:
+            #     cheater = True
+            #     print("Cheater! or maybe you mistyped something?")
+
+            # Convert response to list of string-digits, then convert those string digits to ints.
+            held_dice = [*response]
+            held_dice = list(map(int, held_dice))
+
+            # Remove the number of dice taken from the die count.
+            die_count -= len(held_dice)
+
+            # Calculate this hand's score from the dice currently held
+            hand_score = GameLogic.calculate_score(held_dice)
+
+            print(
+                f"You have {hand_score} unbanked points and {die_count} dice remaining"
+            )
+
+            print("(r)oll again, (b)ank your points or (q)uit:")
+            response = input("> ")
+
+            if response == "q":
+                print(f"Thanks for playing. You earned {total_score} points")
+                exit()
+
+            # None of the provided sim files ever press r at this point, so it remains unimplemented for version 2.
+            if response == "r":
+                print("Not implemented!")
+                response = "b"
+
+            if response == "b":
+                print(f"You banked {hand_score} points in round {round_count}")
+
+                # update total score, reset unbanked points, increment round, reset die count.
+                total_score += hand_score
+                hand_score = 0
+                round_count += 1
+                die_count = 6
+
+                print(f"Total score is {total_score} points")
+
+
+GameLogic.startup_prompt()
+GameLogic.play_game()
